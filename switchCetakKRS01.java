cc97dafa67598d7c66641d58acaba8fbf8ccd6d4
praditadf <achmadpradita17@gmail.com>
2025-09-30 10:20:22 +0700

    Percobaan 2

---
+++ switchCetakKRS01.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class switchCetakKRS01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---Cetak KRS SIAKAD---");
+        System.out.println("Masukkan Semester saat ini: ");
+        int semester=sc.nextInt();
+
+        switch (semester) {
+            case 1:
+            System.out.println("Krs Semester 1 ditampilkan");
+            break;
+            case 2:
+            System.out.println("Krs Semester 2 ditampilkan");
+            break;
+            case 3:
+            System.out.println("Krs Semester 3 ditampilkan");
+            break;
+            case 4:
+            System.out.println("Krs Semester 4 ditampilkan");
+            break;
+            case 5:
+            System.out.println("Krs Semester 5 ditampilkan");
+            break;
+            case 6:
+            System.out.println("Krs Semester 6 ditampilkan");
+            break;
+            case 7:
+            System.out.println("Krs Semester 7 ditampilkan");
+            break;
+            case 8:
+            System.out.println("Krs Semester 8 ditampilkan");
+            break;
+            default:
+            System.out.println("Semester tidak valid");
+        }
+    }
+}
