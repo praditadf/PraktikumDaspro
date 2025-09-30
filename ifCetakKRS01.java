ece60cb9e5d732443f251aa8c10077246830eaec
praditadf <achmadpradita17@gmail.com>
2025-09-30 18:03:12 +0700

    Tugas 1

--- ifCetakKRS01.java
+++ ifCetakKRS01.java
@@ -5,13 +5,8 @@ public class ifCetakKRS01 {
         System.out.println("---Cetak KRS SIAKAD---");
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
-        
-        if(uktLunas){
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        }
-        else{
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        String pesan;
+        pesan = (uktLunas) ? "Pembayaran UKT terverifikasi,\nSilahkan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+        System.out.println(pesan);
     }
 }
