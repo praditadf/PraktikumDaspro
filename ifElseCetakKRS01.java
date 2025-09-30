793be8063c69742dbfd7e53c666e0604a4ae9ada
praditadf <achmadpradita17@gmail.com>
2025-09-30 10:35:30 +0700

    Modifikasi Percobaan 2

---
+++ ifElseCetakKRS01.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class ifElseCetakKRS01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---Cetak KRS SIAKAD---");
+        System.out.println("Masukkan Semester saat ini: ");
+        int semester=sc.nextInt();
+
+        if (semester==1){
+            System.out.println("Krs Semester 1 ditampilkan");
+        }else if(semester==2){
+            System.out.println("Krs Semester 2 ditampilkan");
+        }else if(semester==3){
+            System.out.println("Krs Semester 3 ditampilkan");
+        }else if(semester==4){
+            System.out.println("Krs Semester 4 ditampilkan");
+        }else if(semester==5){
+            System.out.println("Krs Semester 5 ditampilkan");
+        }else if(semester==6){
+            System.out.println("Krs Semester 6 ditampilkan");
+        }else if(semester==7){
+            System.out.println("Krs Semester 7 ditampilkan");
+        }else if(semester==8){
+            System.out.println("Krs Semester 8 ditampilkan");
+        }
+        else{
+            System.out.println("Semester tidak valid");
+        }
+    }
+}
