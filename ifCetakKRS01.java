105fe399ce37053037d3ae717c268c390bff4414
praditadf <achmadpradita17@gmail.com>
2025-09-30 09:58:07 +0700

    Modifikasi Percobaan 1

--- ifCetakKRS01.java
+++ ifCetakKRS01.java
@@ -10,5 +10,8 @@ public class ifCetakKRS01 {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
         }
+        else{
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        }
     }
 }
