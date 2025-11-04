f6164d14852f547ae0571ddc61ce74462fb9fb2c
praditadf <achmadpradita17@gmail.com>
2025-11-04 10:55:12 +0700

    Modifikasi Percobaan 2

--- jobsheet9/ArrayNilai01.java
+++ jobsheet9/ArrayNilai01.java
@@ -4,12 +4,16 @@ public class ArrayNilai01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int [] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Nilai akhir ke-"+i+" lulus!");
+            }else {
+                System.out.println("Nilai akhir ke-"+i+" tidak lulus!");
+            }
         }
     }
 }
