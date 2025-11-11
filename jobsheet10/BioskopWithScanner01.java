fcdd4f54a5caba6db8d0d678b8c3a1128d9f8bf9
praditadf <achmadpradita17@gmail.com>
2025-11-11 11:20:16 +0700

    Modifikasi Percobaan 2

--- jobsheet10/BioskopWithScanner01.java
+++ jobsheet10/BioskopWithScanner01.java
@@ -3,24 +3,59 @@ import java.util.Scanner;
 public class BioskopWithScanner01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int baris, kolom;
+        int baris, kolom, menu;
         String nama, next;
         String [][] penonton = new String[4][2];
         while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
+            System.out.println("Menu 1: Input data penonton");
+            System.out.println("Menu 2: Tampilkan data penonton");
+            System.out.println("Menu 3: Exit");
+            System.out.print("Masukkan menu: ");
+            menu = sc.nextInt();
             sc.nextLine();
+            switch (menu) {
+                case 1:
+                while (true) {
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
+                    System.out.print("Masukkan baris: ");
+                    baris = sc.nextInt();
+                    System.out.print("Masukkan kolom: ");
+                    kolom = sc.nextInt();
+                    sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+                    if (penonton[baris-1][kolom-1] == null) {
+                        System.out.println("Kursi yang dipilih tersedia");
+                    } else if (penonton[baris-1][kolom-1] != null) {
+                        System.out.println("Kursi yang dipilih sudah terisi oleh penonton lainnya");
+                        continue;
+                    }
+                    penonton[baris-1][kolom-1] = nama;
+                    
+                    System.out.print("Input penonton lainnya? (y/n): ");
+                    next = sc.nextLine();
+                    if (next.equalsIgnoreCase("n")) {
+                        break;
+                    }
+                }System.out.println();
+                break;
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+                case 2:
+                for (String []temp : penonton) {
+                    for (String dataPenonton : temp) {
+                        if (dataPenonton == null) {
+                            System.out.println("***");
+                        }else{
+                            System.out.println(dataPenonton + " ");
+                        }
+                    }
+                }System.out.println();
+                break;
+                
+                case 3:
+                return; 
 
-            if (next.equalsIgnoreCase("n")) {
+                default:
                 break;
             }
         }
