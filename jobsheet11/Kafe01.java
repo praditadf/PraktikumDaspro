b9acf0d96b62ff27aed457552998b30850835f6e
praditadf <achmadpradita17@gmail.com>
2025-11-28 18:12:25 +0700

    Percobaan 3

--- jobsheet11/Kafe01.java
+++ jobsheet11/Kafe01.java
@@ -1,5 +1,7 @@
 package jobsheet11;
+import java.util.Scanner;
 
+import jobsheet7.tiketBioskop;
 public class Kafe01 {
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo){
         System.out.println("Selamat datang, "+namaPelanggan+"!");
@@ -24,8 +26,20 @@ public class Kafe01 {
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
     }
 
+    public static int hitungTotalHarga01 (int pilihanMenu, int banyakItem){
+        int [] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+        int hargaTotal = hargaItems[pilihanMenu-1] * banyakItem;
+        return hargaTotal;
+    }
+
     public static void main(String[] args) {
-        Menu("Andi", true, "DISKON50");
+        Scanner sc = new Scanner(System.in);
         Menu("Budi", true, "DISKON30");
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu=sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem=sc.nextInt();
+        int totalHarga = hitungTotalHarga01(pilihanMenu, banyakItem);
+        System.out.println("Total harga untuk pesanan anda: Rp"+totalHarga);
     }
 }
