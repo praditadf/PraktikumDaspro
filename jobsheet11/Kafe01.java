6cda2f61cc9588805926654c938d9e1ecc49bd3c
praditadf <achmadpradita17@gmail.com>
2025-11-28 17:54:17 +0700

    Modifikasi Percobaan 2

--- jobsheet11/Kafe01.java
+++ jobsheet11/Kafe01.java
@@ -1,10 +1,17 @@
 package jobsheet11;
 
 public class Kafe01 {
-    public static void Menu(String namaPelanggan, boolean isMember){
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo){
         System.out.println("Selamat datang, "+namaPelanggan+"!");
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Anda mendapatkan diskon 50%!");
+        }else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Anda mendapatkan diskon 30%!");
+        }else {
+            System.out.println("Kode promo invalid");
         }
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
@@ -18,6 +25,7 @@ public class Kafe01 {
     }
 
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Andi", true, "DISKON50");
+        Menu("Budi", true, "DISKON30");
     }
 }
