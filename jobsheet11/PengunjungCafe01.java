97f220dde7ae18b3e79a572cd16ea72d101d2ea5
praditadf <achmadpradita17@gmail.com>
2025-11-30 20:03:01 +0700

    Modifikasi Percobaan 4

--- jobsheet11/PengunjungCafe01.java
+++ jobsheet11/PengunjungCafe01.java
@@ -8,8 +8,8 @@ public class PengunjungCafe01 {
     }
     static void daftarPengunjung(String... namaPengunjung){
         System.out.println("Daftar nama pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- "+namaPengunjung[i]);
+        for (String i : namaPengunjung) {
+            System.out.println("- "+i);
         }
     }
 }
